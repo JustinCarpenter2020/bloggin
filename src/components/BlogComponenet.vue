5cb4946fbfdafbe6eced9c3748b85265bc1f5b41
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-01-22 11:49:14 -0700

    link clickable

--- src/components/BlogComponenet.vue
+++ src/components/BlogComponenet.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="BlogComponenet">
-    <h1>Hello From Componenet</h1>
+    <div class="col">
+      <div class="card border-primary">
+        <div class="card-body">
+          <h4 class="card-title">
+            <router-link :to="{
+              name:
+                'BlogDetails',
+              params:
+                {id:
+                  blogProp.id }}"
+            >
+              {{ blogProp.title }}
+            </router-link>
+          </h4>
+          <p class="card-text">
+            {{ blogProp.creator.name }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'BlogComponenet',
-  setup() {
+  props: { blogProp: { type: Object, required: true } },
+  setup(props) {
     return {}
   },
   components: {}
@@ -15,5 +35,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.border{
+ border-radius: 8%;
+}
 </style>
